82b0abfa909ed4c5c21562515eafff0f86307f84
iamgp21 <gaupande21@gmail.com>
2025-07-09 11:44:54 +0530

    Added Code

---
+++ main.py
@@ -0,0 +1,22 @@
+from pymemcache.client.base import Client
+
+
+def main():
+    client = Client(('127.0.0.1', 11211))
+    client.set('Environment', 'wsl2', expire=60) ## expire is in seconds
+    client.set('kv-store', 'memcached', expire=60) ## expire is in seconds
+    result = client.get_many(['Environment', 'Kv-store'])
+    #print(type(result))
+    #print(result)
+    for key, value in result.items():
+        if key is None or value is None:
+            print("Key or value is None, skipping...")
+            continue
+        else:
+            print(f"Key: {key}", end="\n")
+            print(f"Value: {str(value.decode('utf-8'))}", end="\n")
+    #client.flush_all()
+    client.close()
+
+if __name__ == "__main__":
+    main()
